388d27ad82cc15b9781df28f27fbfe49006f5b71
lhache <louis.hache.dev@protonmail.com>
2022-03-27 20:19:08 +0200

    add correction

--- flutter_application_1/lib/utils.dart
+++ flutter_application_1/lib/utils.dart
@@ -1,7 +1,108 @@
+import 'dart:developer';
+
+enum WordValidationStatus { goodPosition, wrongPosition, ignored, notInWord }
+
 bool wordIsAcceptable(String word) {
   return true;
 }
 
 bool wordHasCorrectLength(String wordToFind, String proposal) {
   return wordToFind.length == proposal.length;
+}
+
+List<bool> positionsOfLetterInWord(List<String> word, String letter) {
+  List<bool> matchingPositions =
+      List<bool>.generate(word.length, (index) => false);
+  word.asMap().forEach((i, letterToFind) {
+    matchingPositions[i] = !!(letterToFind == letter);
+  });
+  return matchingPositions;
+}
+
+bool letterIsWellPositionedSomewhereElse(List<String> wordToFind,
+    List<String> givenWord, String letter, int currentIndex) {
+  List<bool> matchingPositions =
+      List<bool>.generate(wordToFind.length, (index) => false);
+  matchingPositions.asMap().forEach((i, value) {
+    if (i == currentIndex) {
+      matchingPositions[i] = false;
+    } else {
+      matchingPositions[i] =
+          !!(wordToFind[i] == letter && wordToFind[i] == givenWord[i]);
+    }
+  });
+
+  return matchingPositions.where((element) => element == true).isEmpty
+      ? false
+      : true;
+}
+
+List<WordValidationStatus> getColorMapCorrection(
+    String wordToFind, String givenWord) {
+  final wordtoFindLetters = wordToFind.split('');
+  final givenWordLetters = givenWord.split('');
+
+// init everything as notInWord
+  List<WordValidationStatus> statusMap = List<WordValidationStatus>.generate(
+      wordtoFindLetters.length, (index) => WordValidationStatus.notInWord);
+
+  // set all good positions
+  givenWordLetters.asMap().forEach((i, letter) {
+    if (letter == wordtoFindLetters[i]) {
+      statusMap[i] = WordValidationStatus.goodPosition;
+    }
+  });
+
+// deal with the ones that are in word but not at the good position
+  givenWordLetters.asMap().forEach((i, letter) {
+    if (wordtoFindLetters.contains(letter) &&
+        statusMap[i] != WordValidationStatus.goodPosition) {
+      int amountInWordToFind = wordtoFindLetters
+          .where((letterToFind) => letterToFind == letter)
+          .length;
+      int amountInGivenWord = givenWordLetters
+          .where((letterToFind) => letterToFind == letter)
+          .length;
+
+      if (amountInWordToFind < amountInGivenWord) {
+        final positionsInGivenWord =
+            positionsOfLetterInWord(givenWordLetters, letter);
+        final positionsBeforeInGivenWord = positionsInGivenWord.sublist(0, i);
+        final positionsAfterInGivenWord = positionsInGivenWord.sublist(i + 1);
+        final amountBefore = positionsBeforeInGivenWord
+            .where((element) => element == true)
+            .length;
+        final amountAFter = positionsAfterInGivenWord
+            .where((element) => element == true)
+            .length;
+        if (amountBefore == 0) {
+          if (letterIsWellPositionedSomewhereElse(
+              wordtoFindLetters, givenWordLetters, letter, i)) {
+            statusMap[i] = WordValidationStatus.ignored;
+          } else {
+            statusMap[i] = WordValidationStatus.wrongPosition;
+          }
+        } else if (amountAFter > 0) {
+          if (amountBefore < amountInWordToFind) {
+            statusMap[i] = WordValidationStatus.wrongPosition;
+          } else {
+            statusMap[i] = WordValidationStatus.ignored;
+          }
+        } else if (amountAFter == 0) {
+          statusMap[i] = WordValidationStatus.ignored;
+        }
+      } else if (amountInWordToFind == amountInGivenWord) {
+        if (letterIsWellPositionedSomewhereElse(
+            wordtoFindLetters, givenWordLetters, letter, i)) {
+          statusMap[i] = WordValidationStatus.ignored;
+        } else {
+          statusMap[i] = WordValidationStatus.wrongPosition;
+        }
+      } else if (amountInWordToFind > amountInGivenWord) {
+        statusMap[i] = WordValidationStatus.wrongPosition;
+      }
+    }
+  });
+
+  return statusMap;
 }
---
+++ flutter_application_1/test/utils_test.dart
@@ -0,0 +1,124 @@
+import 'package:flutter_application_1/utils.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('letterIsWellPositionedSomewhereElse', () {
+    test('case not found: [prout - puuot - o - 3]', () {
+      final wordToFind = 'prout'.split('');
+      final givenWord = 'puuut'.split('');
+      const letter = 'o';
+      const index = 3;
+      expect(
+          letterIsWellPositionedSomewhereElse(
+              wordToFind, givenWord, letter, index),
+          false);
+    });
+    test('case found at other position: [prout - prout - o - 3]', () {
+      final wordToFind = 'prout'.split('');
+      final givenWord = 'prout'.split('');
+      const letter = 'o';
+      const index = 3;
+      expect(
+          letterIsWellPositionedSomewhereElse(
+              wordToFind, givenWord, letter, index),
+          true);
+    });
+    test('case found at same position: [prout - prout - o - 4]', () {
+      final wordToFind = 'prout'.split('');
+      final givenWord = 'prout'.split('');
+      const letter = 'o';
+      const index = 2;
+      expect(
+          letterIsWellPositionedSomewhereElse(
+              wordToFind, givenWord, letter, index),
+          false);
+    });
+    test('case not found at right position: [prout - puuot - u - 1]', () {
+      final wordToFind = 'prout'.split('');
+      final givenWord = 'puuot'.split('');
+      const letter = 'u';
+      const index = 1;
+      expect(
+          letterIsWellPositionedSomewhereElse(
+              wordToFind, givenWord, letter, index),
+          false);
+    });
+    test('case found at right position: [prout - puuot - u - 1]', () {
+      final wordToFind = 'prout'.split('');
+      final givenWord = 'puuut'.split('');
+      const letter = 'u';
+      const index = 1;
+      expect(
+          letterIsWellPositionedSomewhereElse(
+              wordToFind, givenWord, letter, index),
+          true);
+    });
+  });
+  group('color correction', () {
+    test('prout - puuut = [oiioo]', () {
+      const wordToFind = 'prout';
+      const givenWord = 'puuut';
+      final expectedResult = [
+        WordValidationStatus.goodPosition,
+        WordValidationStatus.ignored,
+        WordValidationStatus.ignored,
+        WordValidationStatus.goodPosition,
+        WordValidationStatus.goodPosition
+      ];
+      final actualResult = getColorMapCorrection(wordToFind, givenWord);
+      expect(actualResult, expectedResult);
+    });
+    test('prout - puuot = [ouiuo]', () {
+      const wordToFind = 'prout';
+      const givenWord = 'puuot';
+      final expectedResult = [
+        WordValidationStatus.goodPosition,
+        WordValidationStatus.wrongPosition,
+        WordValidationStatus.ignored,
+        WordValidationStatus.wrongPosition,
+        WordValidationStatus.goodPosition
+      ];
+      final actualResult = getColorMapCorrection(wordToFind, givenWord);
+      expect(actualResult, expectedResult);
+    });
+    test('poter - petee - [oiooi]', () {
+      const wordToFind = 'poter';
+      const givenWord = 'petee';
+      final expectedResult = [
+        WordValidationStatus.goodPosition,
+        WordValidationStatus.ignored,
+        WordValidationStatus.goodPosition,
+        WordValidationStatus.goodPosition,
+        WordValidationStatus.ignored
+      ];
+      final actualResult = getColorMapCorrection(wordToFind, givenWord);
+      expect(actualResult, expectedResult);
+    });
+    test('poter - peete - [ouiui]', () {
+      const wordToFind = 'poter';
+      const givenWord = 'peete';
+      final expectedResult = [
+        WordValidationStatus.goodPosition,
+        WordValidationStatus.wrongPosition,
+        WordValidationStatus.ignored,
+        WordValidationStatus.wrongPosition,
+        WordValidationStatus.ignored
+      ];
+      final actualResult = getColorMapCorrection(wordToFind, givenWord);
+      expect(actualResult, expectedResult);
+    });
+    test('peeti - pomte - [oxxou]', () {
+      const wordToFind = 'peeti';
+      const givenWord = 'pomte';
+      final expectedResult = [
+        WordValidationStatus.goodPosition,
+        WordValidationStatus.notInWord,
+        WordValidationStatus.notInWord,
+        WordValidationStatus.goodPosition,
+        WordValidationStatus.wrongPosition
+      ];
+      final actualResult = getColorMapCorrection(wordToFind, givenWord);
+      expect(actualResult, expectedResult);
+    });
+  });
+}
